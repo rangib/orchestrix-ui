5b492a50668c6b510c6add0dbdf3133dcfa84444
Rangi Biddle <b_rangi@hotmail.com>
2025-09-20 01:33:31 +1000

    test: relocate tests to correct paths and fix vitest config

--- src/vitest.config.ts
+++ src/vitest.config.ts
@@ -1,12 +1,10 @@
 import { defineConfig } from 'vitest/config'
-import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
   test: {
     environment: 'jsdom',
-    setupFiles: ['./src/test/setup.ts'],
+    setupFiles: ['./test/setup.ts'],
     globals: true,
-    include: ['src/**/__tests__/**/*.test.{ts,tsx,js,jsx}']
+    include: ['**/__tests__/**/*.test.{ts,tsx,js,jsx}']
   }
 })
